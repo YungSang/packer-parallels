8725c22f4a3d8a1604cc586f01f121854942500a
YungSang <yungsang@gmail.com>
2014-03-14 12:38:00 +0900

    Add StepCleanFiles to the builder
    - Revise CleanFiles at post-processor as well

--- post-processor/vagrant/parallels.go
+++ post-processor/vagrant/parallels.go
@@ -8,6 +8,10 @@ import (
 	"github.com/mitchellh/packer/packer"
 )
 
+// These are the extensions of files that are unnecessary for the function
+// of a Parallels virtual machine.
+var UnnecessaryFileExtensions = []string{".log", ".backup", ".Backup"}
+
 type ParallelsProvider struct{}
 
 func (p *ParallelsProvider) KeepInputArtifact() bool {
@@ -20,13 +24,19 @@ func (p *ParallelsProvider) Process(ui packer.Ui, artifact packer.Artifact, dir 
 
 	// Copy all of the original contents into the temporary directory
 	for _, path := range artifact.Files() {
-		if extension := filepath.Ext(path); extension == ".log" {
-			continue
+		// If the file isn't critical to the function of the
+		// virtual machine, we get rid of it.
+		// It's done by the builder, but we need one more time
+		// because unregistering a vm creates config.pvs.backup again.
+		unnecessary := false
+		ext := filepath.Ext(path)
+		for _, unnecessaryExt := range UnnecessaryFileExtensions {
+			if unnecessaryExt == ext {
+				unnecessary = true
+				break
+			}
 		}
-		if extension := filepath.Ext(path); extension == ".backup" {
-			continue
-		}
-		if extension := filepath.Ext(path); extension == ".Backup" {
+		if unnecessary {
 			continue
 		}
 
